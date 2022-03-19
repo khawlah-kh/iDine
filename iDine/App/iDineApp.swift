7e9c0133063dc47c896b701909b56eddde6d9059
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2022-03-19 16:21:58 +0300

    Initial Commit

---
+++ iDine/App/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by khawlah khalid on 19/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iDine/View/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by khawlah khalid on 19/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from:"menu.json")
+    var body: some View {
+        NavigationView{
+            
+            List(menu,id:\.id){ section in
+                Section(header: Text("\(section.name)")){
+                    ForEach(section.items,id:\.id){ item in
+                        ItemRow(item: item)
+                        
+                    }
+                    
+                }
+                
+            }
+            .listStyle(.grouped)
+            .navigationTitle("Menu")
+        }
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
